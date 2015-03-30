8a099106cca90615e9dc3b88fe2c58f4f8b752d8
erulabs <seandon.mooy@gmail.com>
2015-03-30 15:34:05 -0500

    helper recipe to bind low port numbers

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'seandon.mooy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures nodejs and iojs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '3.0.0'
+version '3.0.1'
 
 conflicts 'node'
 
---
+++ recipes/bind_low_ports.rb
@@ -0,0 +1,14 @@
+#
+# Author:: Seandon Mooy
+# Cookbook Name:: nodejs
+# Recipe:: bind_low_ports
+#
+
+case node['platform']
+when 'debian', 'ubuntu'
+  package 'libcap2-bin'
+when 'redhat', 'centos', 'fedora', 'amazon', 'scientific'
+  package 'libcap'
+end
+
+execute 'setcap cap_net_bind_service=+ep $(readlink /usr/local/bin/node)'
